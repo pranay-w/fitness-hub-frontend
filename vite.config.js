0f51b2ed2e451a3fc7b48f9000ce6f80cb96607d
pranay-w <pranaywabhitkar6429@gmail.com>
2026-05-08 23:46:54 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/fitness-hub/",
+  base: "/fitness-hub-frontend/",
   plugins: [react()]
 });
